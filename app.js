6ba1b86ed87edfc9e857a4a7f636228df672b567
Arka Sain <arka.sain@aol.com>
2021-04-15 20:12:30 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+/* Core Module Imports */
+const path = require('path');
+
+/* 3rd Party Imports */
+const express = require('express');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+
+/* Import for express-handlebars */
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+/* Registering our (req) body-parser */
+app.use(bodyParser.urlencoded({ extended: false }));
+
+/* Registering our public folder to serve static content/files */
+app.use(express.static(path.join(__dirname, 'public')));
+
+/* Registering our favicon */
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+/* Registering the express-handlebars templating engine */
+app.engine('hbs', expressHbs({ layoutsDir: 'views/layout', defaultLayout: 'main', extname: 'hbs' }));
+
+/* Setting up the global configuration values */
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: '404 - PNF', path: null });
+});
+
+app.listen(3000);
+console.log(`Server running on port 3000 ...`);
